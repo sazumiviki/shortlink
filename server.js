f35a816017e47b78ce03bd56af4614d025ebdb01
Sazumi Viki <146317698+sazumiviki@users.noreply.github.com>
2023-11-19 21:50:32 -0800

    Update server.js

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
